1f5e888a1c19bc1d417f409994963ed2d44c3c50
jarle pedersen <jarle.pedersen@norconsult.com>
2016-12-07 13:05:27 +0100

    fit to property. leave drawing avter deactivate if show is true

--- src/ISY.MapImplementation/OL3/ISY.MapImplementation.OL3.AddLayerUrl.js
+++ src/ISY.MapImplementation/OL3/ISY.MapImplementation.OL3.AddLayerUrl.js
@@ -9,49 +9,63 @@ ISY.MapImplementation.OL3.AddLayerUrl = function(){
     var drawLayer;
     var source = new ol.source.Vector();
     var style;
+    var layerId='propertyMarking';
 
     function _addLayer(map){
         drawLayer = new ol.layer.Vector({
             source: source,
             style: style
         });
+        drawLayer.set('id', layerId);
         map.addLayer(drawLayer);
     }
 
-    var _getSourceFromUrl = function(url, geometryName){
-        if(url) {
+    var _getSourceFromUrl = function(options, map){
+        if(options.url) {
             source = new ol.source.Vector({
                 format: new ol.format.GML(),
-                url: url
+                url: options.url
             });
             source.on('addfeature',
                 function(event){
-                    if(geometryName) {
-                        event.feature.setGeometryName(geometryName);
+                    if(options.geometryName) {
+                        event.feature.setGeometryName(options.geometryName);
                     }
                     event.feature.getGeometry().transform('EPSG:4326', mapProjection);
+                    var extent = source.getExtent();
+                    map.getView().fit(extent,map.getSize());
                 }, this);
         }
         return source;
     };
 
+    function _removeOldLayer(map){
+        map.getLayers().forEach(function (layer) {
+                if (layer.get('id') == layerId) {
+                    map.removeLayer(layer);
+                }
+            }
+        );
+    }
+
     function activate(map, options) {
+        if(!options.show) {
+            map.removeLayer(drawLayer);
+            deactivate();
+            return;
+        }
         isActive = true;
         mapProjection = map.getView().getProjection();
         style = options.style ? options.style : defaultStyle;
-        source = _getSourceFromUrl(options.url, options.geometryName);
+        source = _getSourceFromUrl(options, map);
+        _removeOldLayer(map);
         _addLayer(map);
-        // var extent = features[0].getGeometry().getExtent();
-        // map.getView().fit(extent,map.getSize());
     }
 
-    function deactivate(map){
+    function deactivate(){
         if (isActive) {
             isActive = false;
-            if (map !== undefined) {
-                map.removeLayer(drawLayer);
-                source = new ol.source.Vector();
-            }
+
         }
     }
 
